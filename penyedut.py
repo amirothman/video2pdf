8ab07bd1b4de68789295b82a72badcf5e1021fac
Amir Othman <th3go2@gmail.com>
2016-02-16 15:21:42 +0100

    initial commit

---
+++ penyedut.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+class Downloader:
+    def __init__(self,url):
+        self.url = url
+        self.file_name = ""
+
+    def my_hook(self,d):
+        self.file_name = d['filename']
+ 
+    def download(self):
+        ydl_opts = {
+                        "restrictfilenames":True,
+                        "write_subtitles":True,
+                        "writeinfojson":True,
+                        "progress_hooks":[self.my_hook]
+                      }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            downloaded = ydl.download([self.url])
+        return self.file_name
+
+downloader = Downloader("https://www.youtube.com/watch?v=S091lKYWbSs")
+filename = downloader.download()
+print(filename)
---
+++ proses_video.py
@@ -0,0 +1,54 @@
+import copy
+import cv2
+from scipy import spatial
+import numpy as np
+
+sourceVideo = "Sutherland-Hodgman_Polygon_Clipping_Algorithm-S091lKYWbSs.mp4"
+cam = cv2.VideoCapture(sourceVideo)
+
+_count = 0
+_buffer = None
+
+orb = cv2.ORB_create()
+bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+arr = []
+arr_frame = []
+while True:
+    ret, img = cam.read()                      
+    if (type(img) == type(None)):
+        break
+    if (0xFF & cv2.waitKey(5) == 27) or img.size == 0:
+        break
+    #print(count)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    if _buffer is not None:
+        if _count % 30 == 0:
+            #do distance computation
+            #_distance = spatial.distance.cosine(img,_buffer)
+            #print(img)
+            #print(_distance)
+            #_distance = (np.linalg.norm(np.dot(img,_buffer.T)))
+            #print("distance",_distance)
+            #print("frame_count",_count)
+            kp1, des1 = orb.detectAndCompute(_buffer,None)
+            kp2, des2 = orb.detectAndCompute(img,None)
+            #print(_count,des1,des2)
+            if des1 is not None and des2 is not None:
+                matches = bf.match(des1,des2)
+                #print(np.mean([match.distance for match in matches])*len(matches),_count)
+                arr.append(np.mean([match.distance for match in matches])*len(matches))
+                print(_count)
+                arr_frame.append(_count)
+
+            _buffer = copy.deepcopy(img)
+    else:
+        _buffer = copy.deepcopy(img)
+    #print(ret)
+    _count +=1
+
+top_distances = np.percentile(arr,90)
+
+distances = [arr.index(distance) for distance in arr if distance > top_distances]
+
+images = [arr_frame[idx]/30 for idx in distances]
+print(images)
